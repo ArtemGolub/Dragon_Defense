8ebbd0f45104e7af2c0aecc9a6105aacf9b81207
otodraliti <otodraliti@gmail.com>
2023-09-21 16:22:06 +0300

    feat: Player Health MVC, Wave Controller, Event Manager, Wave Spawner

---
+++ Assets/_InternalAssets/Scripts/MVC/HealthPoints/HPController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPController : MonoBehaviour
+{
+    public static HPController intstance;
+
+    private HpModel _hpModel;
+    private IHPView hpView;
+
+    private void Awake()
+    {
+        intstance = this;
+
+        _hpModel = new HpModel();
+        hpView = GetComponent<IHPView>();
+        hpView.Initialize(this);
+    }
+
+    public void ShowUI()
+    {
+        hpView.ShowUI();
+    }
+
+    public void UpdateText(int currentHp)
+    {
+        hpView.UpdateUI(currentHp);
+    }
+}
